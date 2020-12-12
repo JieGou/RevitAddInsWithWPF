e5ba278bf0b4a42bd066240f8a1ecdcd846471cf
陈杰 <jiegou04@gmail.com>
2020-12-12 21:50:17 +0800

    No change。添加真正跳转视图任务

--- RevitAddInsWPFSample/RevitAddInsWPFSample/Viewer.xaml.cs
+++ RevitAddInsWPFSample/RevitAddInsWPFSample/Viewer.xaml.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Point = System.Windows.Point;
 
 namespace RevitAddInsWPFSample
 {
@@ -65,7 +68,16 @@ namespace RevitAddInsWPFSample
                 // view
                 View view = element as View;
                 // view typename
-                viewTypenames.Add(view?.ViewType.ToString());
+                //string viewTypeName = view?.ViewType.ToString();
+
+                //直接改为使用参数获取与当前语言相关的名字
+                string viewTypeName = view?.get_Parameter(BuiltInParameter.VIEW_TYPE).AsString();
+
+                if (string.IsNullOrEmpty(viewTypeName))
+                {
+                    continue;
+                }
+                viewTypenames.Add(viewTypeName);
             }
 
             // create treeviewitem for viewtype
@@ -86,16 +98,100 @@ namespace RevitAddInsWPFSample
                 // viewname
                 string itemViewName = view?.Name;
                 // view typename
-                string viewTypename = view?.ViewType.ToString();
+                //string viewTypeName = view?.ViewType.ToString();
+                string viewTypeName = view?.get_Parameter(BuiltInParameter.VIEW_TYPE).AsString();
+
+                if (string.IsNullOrEmpty(viewTypeName))
+                {
+                    continue;
+                }
+
                 // create view treeviewitem
                 if (itemViewName != null)
                 {
                     TreeViewItem viewItem = new TreeViewItem() { Header = itemViewName };
 
                     // view item add to view type
-                    viewTypeDictionary[viewTypename].Items.Add(viewItem);
+                    viewTypeDictionary[viewTypeName].Items.Add(viewItem);
                 }
             }
+        }
+
+        private void TreeViewItem_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter && this.treeview.SelectedItem != null)
+            {
+                this.Button_Click(sender, e);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource is TreeViewItem item)
+            {
+                JumpView(item);
+            }
+
+            e.Handled = true;
+        }
+
+        private void JumpView(TreeViewItem item)
+        {
+            var header = item.Header;
+
+            ItemsControl parent = GetSelectedTreeViewItemParent(item);
+
+            TreeViewItem treeitem = parent as TreeViewItem;
+
+            string parantHeader = treeitem?.Header.ToString();
+
+            //跳转到指定视图
+            MessageBox.Show($"跳转到指定视图 => {parantHeader + ": " + header}");
+        }
+
+        public ItemsControl GetSelectedTreeViewItemParent(TreeViewItem item)
+        {
+            DependencyObject parent = VisualTreeHelper.GetParent(item);
+            while (!(parent is TreeViewItem || parent is TreeView))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parent as ItemsControl;
+        }
+
+        private void TreeViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            //奇怪 这里获取得到的节点树
+            if (sender is TreeViewItem item)
+            {
+                var parantHeader = item.Header;
+
+                var clickedItem = TryGetClickedItem(this.treeview, e);
+                var itemHeader = clickedItem.Header;
+
+                //TODO 跳转视图
+                MessageBox.Show($"跳转到指定视图 => {parantHeader + ": " + itemHeader}");
+            }
+
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// 获取双击选择的项
+        /// </summary>
+        /// <param name="treeView"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        private TreeViewItem TryGetClickedItem(TreeView treeView, MouseButtonEventArgs e)
+        {
+            var hit = e.OriginalSource as DependencyObject;
+            while (hit != null && !(hit is TreeViewItem))
+            {
+                hit = VisualTreeHelper.GetParent(hit);
+            }
+
+            return hit as TreeViewItem;
         }
     }
 }
