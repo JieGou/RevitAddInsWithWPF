6e451ec5a2a201d90a0a0b06370e5fd67240e706
陈杰 <jiegou04@gmail.com>
2020-12-12 16:48:01 +0800

    1、去除Register命令注册，改为在启动时自动注册

---
+++ RevitAddInsWPFSample/RevitAddInsWPFSample/MainClass.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+using Autodesk.Revit.Attributes;
+
+namespace RevitAddInsWPFSample
+{
+    // external application class
+    public class MainClass : IExternalApplication
+    {
+        private Viewer _dockableWindow = null;
+        private Document _doc;
+
+        /// <summary>
+        /// 插件入口
+        /// </summary>
+        /// <param name="application"></param>
+        /// <returns></returns>
+        // execute when application open
+        public Result OnStartup(UIControlledApplication application)
+        {
+            #region 注册可停靠窗体
+
+            // dockable window
+            Viewer dock = new Viewer();
+            _dockableWindow = dock;
+
+            // create a new dockable pane id
+            DockablePaneId id = new DockablePaneId(new Guid("{68D44FAC-CF09-46B2-9544-D5A3F809373C}"));
+            try
+            {
+                // register dockable pane
+                application.RegisterDockablePane(id, "TwentyTwo Dockable Sample", _dockableWindow as IDockablePaneProvider);
+
+#if DEBUG
+                TaskDialog.Show("Info Message", "Dockable window have registered!");
+#endif
+
+                // subscribe view activated event
+                application.ViewActivated += new EventHandler<ViewActivatedEventArgs>(Application_ViewActivated);
+            }
+            catch (Exception ex)
+            {
+                // show error info dialog
+                TaskDialog.Show("Info Message", ex.Message);
+            }
+
+            #endregion 注册可停靠窗体
+
+            // create a ribbon panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(Tab.AddIns, "TwentyTwo Sample");
+            // assembly
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            // assembly path
+            string assemblyPath = assembly.Location;
+
+            // Create Show Button
+            PushButton showButton = ribbonPanel.AddItem(new PushButtonData("Show Window", "Show", assemblyPath,
+                "RevitAddInsWPFSample.Show")) as PushButton;
+            // btn tooltip
+            showButton.ToolTip = "Show the registered dockable window.";
+            // show button icon images
+            showButton.LargeImage = GetResourceImage(assembly, "RevitAddInsWPFSample.Resources.show32.png");
+            showButton.Image = GetResourceImage(assembly, "RevitAddInsWPFSample.Resources.show16.png");
+
+            // return status
+            return Result.Succeeded;
+        }
+
+        // execute when application close
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // return status
+            return Result.Succeeded;
+        }
+
+        // get embedded images from assembly resources
+        public ImageSource GetResourceImage(Assembly assembly, string imageName)
+        {
+            try
+            {
+                // bitmap stream to construct bitmap frame
+                Stream resource = assembly.GetManifestResourceStream(imageName);
+                // return image data
+                return BitmapFrame.Create(resource);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        // view activated event
+        public void Application_ViewActivated(object sender, ViewActivatedEventArgs e)
+        {
+            _doc = e.CurrentActiveView.Document;
+            // provide ExternalCommandData object to dockable page
+            _dockableWindow.CustomInitiator(_doc);
+        }
+    }
+
+    // external command availability
+    public class CommandAvailability : IExternalCommandAvailability
+    {
+        // interface member method
+        public bool IsCommandAvailable(UIApplication app, CategorySet cate)
+        {
+            // zero doc state
+            if (app.ActiveUIDocument == null)
+            {
+                // disable register btn
+                return true;
+            }
+            // enable register btn
+            return false;
+        }
+    }
+
+    // external command class
+    [Transaction(TransactionMode.Manual)]
+    public class Show : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                // dockable window id
+                DockablePaneId id = new DockablePaneId(new Guid("{68D44FAC-CF09-46B2-9544-D5A3F809373C}"));
+                DockablePane dockableWindow = commandData.Application.GetDockablePane(id);
+                dockableWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                // show error info dialog
+                TaskDialog.Show("Info Message", ex.Message);
+            }
+            // return result
+            return Result.Succeeded;
+        }
+    }
+}
--- RevitAddInsWPFSample/RevitAddInsWPFSample/Viewer.xaml.cs
+++ RevitAddInsWPFSample/RevitAddInsWPFSample/Viewer.xaml.cs
@@ -1,54 +1,80 @@
-﻿using System;
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using Autodesk.Revit.DB;
 
 namespace RevitAddInsWPFSample
 {
     /// <summary>
     /// Interaction logic for Viewer.xaml
     /// </summary>
-    public partial class Viewer : Window
+    public partial class Viewer : Page, IDockablePaneProvider
     {
-        // fields
-        public Document document;
-        // constructor
-        public Viewer(Document doc)
+        public Document doc = null;
+
+        // IDockablePaneProvider abstrat method
+        public void SetupDockablePane(DockablePaneProviderData data)
         {
-            // assign value to field
-            document = doc;
+            // wpf object with pane's interface
+            data.FrameworkElement = this as FrameworkElement;
+            // initial state position
+            data.InitialState = new DockablePaneState
+            {
+                DockPosition = DockPosition.Tabbed,
+                TabBehind = DockablePanes.BuiltInDockablePanes.ProjectBrowser
+            };
+        }
+
+        // constructor
+        public Viewer()
+        {
             InitializeComponent();
-            // display view
+        }
+
+        // custom initiator
+        public void CustomInitiator(Document doc)
+        {
+            this.doc = doc;
+
+            // get the current document name
+            docName.Text = doc.PathName.ToString().Split('\\').Last();
+            // get the active view name
+            viewName.Text = doc.ActiveView.Name;
+            // call the treeview display method
             DisplayTreeViewItem();
         }
 
         public void DisplayTreeViewItem()
         {
+            // clear items first
+            treeview.Items.Clear();
+
             // viewtypename and treeviewitem dictionary
-            SortedDictionary<string,TreeViewItem> ViewTypeDictionary = new SortedDictionary<string, TreeViewItem>();
+            SortedDictionary<string, TreeViewItem> viewTypeDictionary = new SortedDictionary<string, TreeViewItem>();
             // viewtypename
             List<string> viewTypenames = new List<string>();
-            
+
             // collect view type
-            List<Element> elements = new FilteredElementCollector(document).OfClass(typeof(View)).ToList();
-            
+            List<Element> elements = new FilteredElementCollector(doc).OfClass(typeof(View)).ToList();
+
             foreach (Element element in elements)
             {
                 // view
                 View view = element as View;
                 // view typename
-                viewTypenames.Add(view.ViewType.ToString());
+                viewTypenames.Add(view?.ViewType.ToString());
             }
-                      
+
             // create treeviewitem for viewtype
             foreach (string viewTypename in viewTypenames.Distinct().OrderBy(name => name).ToList())
             {
                 // create viewtype treeviewitem
                 TreeViewItem viewTypeItem = new TreeViewItem() { Header = viewTypename };
                 // store in dict
-                ViewTypeDictionary[viewTypename] = viewTypeItem;
+                viewTypeDictionary[viewTypename] = viewTypeItem;
                 // add to treeview
                 treeview.Items.Add(viewTypeItem);
             }
@@ -58,13 +84,17 @@ namespace RevitAddInsWPFSample
                 // view
                 View view = element as View;
                 // viewname
-                string viewName = view.Name;
+                string itemViewName = view?.Name;
                 // view typename
-                string viewTypename = view.ViewType.ToString();          
-                // create view treeviewitem 
-                TreeViewItem viewItem = new TreeViewItem() { Header = viewName };
-                // view item add to view type
-                ViewTypeDictionary[viewTypename].Items.Add(viewItem);
+                string viewTypename = view?.ViewType.ToString();
+                // create view treeviewitem
+                if (itemViewName != null)
+                {
+                    TreeViewItem viewItem = new TreeViewItem() { Header = itemViewName };
+
+                    // view item add to view type
+                    viewTypeDictionary[viewTypename].Items.Add(viewItem);
+                }
             }
         }
     }
